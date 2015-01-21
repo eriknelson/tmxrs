830cf8593510add33b7ea88ba55f6f167fffb360
Erik Nelson <erik@nelsk.com>
2015-01-21 01:00:36 -0500

    Bring in rsfml and setup initial tileinfo struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+
+extern crate rsfml;
+
 pub use self::map_loader::MapLoader;
 pub use self::map_layer::MapLayer;
 pub use self::map_object::MapObject;
--- src/map_loader.rs
+++ src/map_loader.rs
@@ -1,5 +1,14 @@
-use super::MapOrientation;
 use std::collections::HashMap;
+use std::default::Default;
+
+//use core::num::from_i32;
+
+use rsfml::system::Vector2f;
+use rsfml::graphics::{
+  IntRect
+};
+
+use super::MapOrientation;
 
 pub struct MapLoader {
   // Tile count
@@ -14,4 +23,40 @@ pub struct MapLoader {
 
   // Width / height isometric tiles
   tile_ratio: f32,
+}
+
+// Holds texture coords and tileset id of a tile
+struct TileInfo {
+  coords: [Vector2f; 4],
+  size: Vector2f,
+  tile_set_id: u16,
+
+}
+
+impl TileInfo {
+  fn defaultNew() -> TileInfo {
+    TileInfo {
+      coords: [Vector2f::new(0., 0.); 4],
+      size: Vector2f::new(0., 0.),
+      tile_set_id: 0_u16,
+    }
+  }
+  fn new(rect: &IntRect,
+         size: &Vector2f,
+         tile_set_id: u16) -> TileInfo
+  {
+    //coords: [Vector2f::new(rect.left, rect.top)]
+    TileInfo::defaultNew()
+  }
+}
+
+#[cfg(test)]
+mod tests{
+  use super::TileInfo;
+
+  #[test]
+  fn hello_test_loader() {
+    println!("Logging");
+    assert!(true);
+  }
 }
