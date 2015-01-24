764fe269019f3ae6d0448d12e2e8b32ec0fb7737
Erik Nelson <erik@nelsk.com>
2015-01-24 16:40:07 -0500

    Add TileInfo Default trait impl and write some tests

--- src/map_loader.rs
+++ src/map_loader.rs
@@ -5,12 +5,13 @@ use std::default::Default;
 
 use rsfml::system::Vector2f;
 use rsfml::graphics::{
-  IntRect
+  IntRect,
+  FloatRect
 };
 
 use super::MapOrientation;
 
-pub struct MapLoader {
+pub struct MapLoader<'a> {
   // Tile count
   width: u16,
   height: u16,
@@ -23,6 +24,11 @@ pub struct MapLoader {
 
   // Width / height isometric tiles
   tile_ratio: f32,
+
+  properties: HashMap<&'a str, &'a str>,
+
+  // Bounding area of tiles visible on screen
+  bounds: FloatRect,
 }
 
 // Holds texture coords and tileset id of a tile
@@ -33,30 +39,63 @@ struct TileInfo {
 
 }
 
-impl TileInfo {
-  fn defaultNew() -> TileInfo {
+impl Default for TileInfo {
+  fn default() -> TileInfo {
     TileInfo {
-      coords: [Vector2f::new(0., 0.); 4],
-      size: Vector2f::new(0., 0.),
+      coords: [
+        Vector2f{ x: 0., y: 0. },
+        Vector2f{ x: 0., y: 0. },
+        Vector2f{ x: 0., y: 0. },
+        Vector2f{ x: 0., y: 0. },
+      ],
+      size: Vector2f{ x: 0., y: 0. },
       tile_set_id: 0_u16,
     }
-  }
-  fn new(rect: &IntRect,
-         size: &Vector2f,
-         tile_set_id: u16) -> TileInfo
-  {
-    //coords: [Vector2f::new(rect.left, rect.top)]
-    TileInfo::defaultNew()
   }
 }
 
 #[cfg(test)]
 mod tests{
   use super::TileInfo;
+  use rsfml::system::Vector2f;
+  use std::default::Default;
 
   #[test]
-  fn hello_test_loader() {
-    println!("Logging");
-    assert!(true);
+  fn test_explicitly_initialized_TileInfo() {
+    let tile_info = TileInfo {
+      coords: [
+        Vector2f{ x:0., y:1. },
+        Vector2f{ x:1., y:2. },
+        Vector2f{ x:3., y:4. },
+        Vector2f{ x:4., y:5. },
+      ],
+      size: Vector2f{ x: 100., y: 200. },
+      tile_set_id: 15_u16
+    };
+
+    assert!(tile_info.coords[0] == Vector2f{ x:0., y:1. });
+    assert!(tile_info.coords[1] == Vector2f{ x:1., y:2. });
+    assert!(tile_info.coords[2] == Vector2f{ x:3., y:4. });
+    assert!(tile_info.coords[3] == Vector2f{ x:4., y:5. });
+    assert!(tile_info.size == Vector2f{ x: 100., y: 200. });
+    assert!(tile_info.tile_set_id == 15_u16);
+  }
+
+  #[test]
+  fn test_default_TileInfo() {
+    let tile_info: TileInfo = Default::default();
+    assert!(tile_info.coords[0] == Vector2f{ x:0., y:0. });
+    assert!(tile_info.coords[1] == Vector2f{ x:0., y:0. });
+    assert!(tile_info.coords[2] == Vector2f{ x:0., y:0. });
+    assert!(tile_info.coords[3] == Vector2f{ x:0., y:0. });
+  }
+
+  #[test]
+  fn test_field_override_default_TileInfo() {
+    let tile_info = TileInfo {
+      size: Vector2f{ x: 22., y: 33. },
+      ..Default::default()
+    };
+    assert!(tile_info.size == Vector2f{ x: 22., y: 33. });
   }
 }
