d5c58d2ebb4f46461497bad96f0c8d982ee371b0
Erik Nelson <erik@nelsk.com>
2015-01-31 20:50:48 -0500

    Segemnt intersection methods

--- src/geo.rs
+++ src/geo.rs
@@ -8,15 +8,15 @@ pub struct Segment {
 }
 
 impl Segment {
-  pub fn intersects(&self, segment: &Segment) -> math::Intersection {
-    return math::Intersection::DoesNotIntersect;
-    //return math::intersects(&self, &segment)
+  pub fn intersects(&self, segment: &Segment) -> bool {
+    return math::intersects(self, segment);
   }
 }
 
 #[cfg(test)]
 mod tests {
   use std::default::Default;
+  use rsfml::system::Vector2f;
   use super::Segment;
 
   #[test]
@@ -24,6 +24,38 @@ mod tests {
     let seg: Segment = Default::default();
     assert!(seg.start == Default::default());
     assert!(seg.end == Default::default());
+  }
+
+  #[test]
+  fn test_segment_method_should_intersect(){
+    let seg_one = Segment {
+      start: Vector2f { x: -3.0, y: 1.0 },
+      end: Vector2f { x: 1.0, y: -2.0}
+    };
+
+    let seg_two = Segment {
+      start: Vector2f { x: 2.0, y: 2.0 },
+      end: Vector2f { x: -1.0, y: -4.0 }
+    };
+
+    assert!(seg_one.intersects(&seg_two));
+    assert!(seg_two.intersects(&seg_one));
+  }
+
+  #[test]
+  fn test_segment_method_should_not_intersect(){
+    let seg_one = Segment {
+      start: Vector2f { x: -3.0, y: 1.0 },
+      end: Vector2f { x: 1.0, y: -2.0}
+    };
+
+    let seg_two = Segment {
+      start: Vector2f { x: 2.0, y: 2.0 },
+      end: Vector2f { x: 2.0, y: -2.0 }
+    };
+
+    assert!(!seg_one.intersects(&seg_two));
+    assert!(!seg_two.intersects(&seg_one));
   }
 
 }
